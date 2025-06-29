9a128e7330eac836b3d34d04d70c6e463154fc15
mayank <mayankkumars584@gmail.com>
2025-06-29 21:15:06 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+async function finding(){
+    const city=document.getElementById("a");
+     const cityName=city.value;
+    const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=3ed39163b3cedc75ba8e26223bc56ca2&units=metric`)
+  const data=await response.json();
+   document.getElementById("result").innerHTML= "temperature" + data.main.temp + "°C";
+   console.log(data);
+
+}
